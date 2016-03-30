7c4fb8f5ec559c200f7002d1825e53276956d82f
jed1337 <j.caychingco@Yahoo.com>
2016-03-30 16:51:59 +0800

    Nb project changes

--- SocketSheepExample2/SocketSheepExample2Client/src/socketsheepexample2client/SocketSheepExample2Client.java
+++ SocketSheepExample2/SocketSheepExample2Client/src/socketsheepexample2client/SocketSheepExample2Client.java
@@ -1,13 +1,16 @@
 package socketsheepexample2client;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.Socket;
 
 public class SocketSheepExample2Client {
    private final static int PORT = 4096;
+   
     public static void main(String[] args) {
       try {
          Socket socket = new Socket("::1", PORT);
+         InputStream input = socket.getInputStream();
       } catch (IOException ex) {
          System.err.println(ex.getMessage());
       }
